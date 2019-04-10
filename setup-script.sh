46a5db1b2a710d8d4fbb148461f7ac6e78c62c92
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2019-04-10 15:04:21 +0300

    Add Ubuntu 18.04 / KDE neon 5.14.3 bootstrap scripts

---
+++ setup-script.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Ubuntu 18.04 / KDE neon 5.14.3 clean setup
+
+sudo apt update
+sudo apt -y install build-essential cmake
+
+# Install Nvidia driver
+sudo apt -y install nvidia-384
+
+# Install git
+sudo apt -y install git
+
+# Compile and install Guake terminal
+cd ~/Downloads
+git clone https://github.com/Guake/guake.git
+cd guake/
+git reset --hard c3ca237bc43cc46ba5f7747e8a5e58a8f657930f
+./scripts/bootstrap-dev-debian.sh run make
+make
+yes | sudo make install
+
+# Install Tilda terminal
+sudo apt install tilda
+# tilda -f "Monospace 12"
+
+# Download and install Google Chrome
+cd ~/Downloads
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt -y install -f
+
+# Download and install Atom
+cd ~/Downloads
+wget https://atom-installer.github.com/v1.35.1/atom-amd64.deb
+sudo dpkg -i atom-amd64.deb
+sudo apt -y install -f
+
+# Download Popcorn Time
+cd ~/Downloads
+mkdir Popcorn
+cd Popcorn/
+wget https://mirror03.popcorntime.sh/repo/build/Popcorn-Time-0.3.10-Linux-64.tar.xz
+tar -xvf Popcorn-Time-0.3.10-Linux-64.tar.xz
+
+# Increase the volume of audio card
+alsamixer
+sudo alsactl store
---
+++ startup-script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Ubuntu 18.04 / KDE neon 5.14.3 mouse and screen tearing fix
+
+# chmod a+x startup-script.sh
+# chmod 777 startup-script.sh
+# sudo cp ./startup-script.sh /etc/init.d/
+# sudo ln -s /etc/init.d/startup-script.sh /etc/rc5.d/
+
+# --- Microsoft Classic Intellimouse ---
+# Fix mouse sensitivity
+xinput --set-prop 8 282 2.0
+# Fix mouse scroll speed
+imwheel --kill --buttons "4 5"
+
+# --- Nvidia GTX Series Graphics Card --
+# Fix screen tearing
+nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
+
+# --- Guake Terminal ---
+# Run Guake on startup
+guake &
+
+# --- Guake Terminal ---
+# Run Guake on startup
+google-chrome &
